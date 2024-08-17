78fde77dc6bc0d4581b1ee9f45e67503e5356a84
ms0242808 <ms0242808@gmail.com>
2024-08-17 12:26:28 +0100

    Update routes

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import quizJson from './/assests/questions/quizzes.json';
 import Landing from './pages/landing';
 import Question from './pages/question';
 import Intro from './pages/intro';
@@ -7,13 +8,13 @@ import Quiz from './pages/quiz';
 import Result from './pages/result';
 
 const AppRoutes = () => (
-  <Routes>
-    <Route path='/' element={<Landing />} />
-    <Route path='/quizzes' element={<Question />} />
-    <Route path='/intro' element={<Intro />} />
-    <Route path='/quiz' element={<Quiz />} />
-    <Route path='/result' element={<Result />} />
-  </Routes>
+	<Routes>
+		<Route path='/' element={<Landing title={quizJson.title} description={quizJson.instructions} />} />
+		<Route path='/quizzes' element={<Question />} />
+		<Route path='/intro' element={<Intro />} />
+		<Route path='/quiz' element={<Quiz questions={quizJson.questions} />} />
+		<Route path='/result' element={<Result result={quizJson.results} />} />
+	</Routes>
 );
 
 export default AppRoutes;
