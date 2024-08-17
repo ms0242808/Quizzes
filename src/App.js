720db1a6a2da66f4b4b9987fb4174d5bec4e365d
ms0242808 <ms0242808@gmail.com>
2024-08-17 12:26:19 +0100

    Added icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import AppRoutes from './AppRoutes';
+import { Bolt } from './components/icons/index';
 import Footer from './components/footer';
 
 function App() {
-  return (
-    <div className='App'>
-      <AppRoutes />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<AppRoutes />
+			<div className='absolute top-4 right-4 text-white'>
+				<Bolt />
+			</div>
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
